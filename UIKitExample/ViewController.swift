e3cf7d1f0c8530de9b130af0134dc5607fa6f03b
firebrick26 <firebrick26@gmail.com>
2017-11-26 09:18:24 +0900

    Initial Commit

---
+++ UIKitExample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UIKitExample
+//
+//  Created by me on 2017/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
